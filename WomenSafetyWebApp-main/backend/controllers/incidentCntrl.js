9406f491e2b9e9503587fb7ba9a86e0838e8cb9f
Pawan Singh <pawan9140582015@gmail.com>
2025-11-16 20:07:18 +0530

    CORS enabled for backend

--- WomenSafetyWebApp-main/backend/controllers/incidentCntrl.js
+++ WomenSafetyWebApp-main/backend/controllers/incidentCntrl.js
@@ -1,36 +1,36 @@
 const asyncHandler = require('express-async-handler');
 const { Incident } = require('../models/incidentRptModel');
-const { User } = require('../models/userModel')
+const { User } = require('../models/userModel');
 
 const fs = require('fs');
-const path = require('path')
-const AWS = require('aws-sdk')
+const path = require('path');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 require('dotenv').config();
 
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-2'
-})
-
-const s3 = new AWS.S3();
+const s3 = new S3Client({
+    region: 'us-east-2',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+});
 
 const addIncident = asyncHandler(async (req, res) => {
 
     const { user, report, pincodeOfIncident, mimeType, address } = req.body;
-    const note= req.file.path
+    const note = req.file.path;
 
-    
-    if(note){
-        const params = {
+    if (note) {
+        const fileStream = fs.createReadStream(req.file.path);
+        const uploadParams = {
             Bucket: process.env.AWS_BUCKET_NAME,
             Key: note,
-            Body: fs.createReadStream(req.file.path),
+            Body: fileStream,
             ContentType: mimeType
-        }
-        const s3Response = await s3.upload(params).promise();
-        const incFile = s3Response.Location;
-       
+        };
+
+        await s3.send(new PutObjectCommand(uploadParams));
+        const incFile = `https://s3.us-east-2.amazonaws.com/${process.env.AWS_BUCKET_NAME}/${note}`;
 
         const incident = await Incident.create({
             user,
@@ -38,41 +38,39 @@ const addIncident = asyncHandler(async (req, res) => {
             pincodeOfIncident,
             address,
             meidaSt: incFile
-        })
+        });
 
-
-
-        if(incident){
-            res.status(201).json({message: "Incident reported successfully"})
-        }else{
-            res.status(500).json({message: "Something went wrong"})
+        if (incident) {
+            res.status(201).json({ message: "Incident reported successfully" });
+        } else {
+            res.status(500).json({ message: "Something went wrong" });
         }
-    }else{
+    } else {
         const incident = await Incident.create({
             user,
             report,
             address,
             pincodeOfIncident
-        })
+        });
 
-        if(incident){
-            res.status(201).json({message: "Incident reported successfully"})
-        }else{
-            res.status(500).json({message: "Something went wrong"})
+        if (incident) {
+            res.status(201).json({ message: "Incident reported successfully" });
+        } else {
+            res.status(500).json({ message: "Something went wrong" });
         }
     }
 
 });
 
 
-const getAllIncidents = asyncHandler(async(req,res) => {
+const getAllIncidents = asyncHandler(async (req, res) => {
 
     const incidents = await Incident.find({});
-    const data = []
-    for(const x of incidents){
+    const data = [];
+    for (const x of incidents) {
         const user = await User.findById(x.user);
-        console.log(user)
-        if(user){
+        console.log(user);
+        if (user) {
             data.push({
                 uname: user.uname,
                 address: x.address,
@@ -82,21 +80,21 @@ const getAllIncidents = asyncHandler(async(req,res) => {
                 image: x.meidaSt || "empty",
                 createdAt: x.createdAt,
                 updatedAt: x.updatedAt
-            })
+            });
         }
-        
+
     }
-    res.status(200).json(data)
+    res.status(200).json(data);
 });
 
-const acknowledgeInc = asyncHandler(async(req,res) => {
+const acknowledgeInc = asyncHandler(async (req, res) => {
 
     const inc = req.params.id;
     const incident = await Incident.findById(inc);
-    if(incident){
+    if (incident) {
         incident.isSeen = true;
-        await incident.save()
+        await incident.save();
     }
-})
+});
 
-module.exports = {addIncident ,getAllIncidents,acknowledgeInc}
+module.exports = { addIncident, getAllIncidents, acknowledgeInc };
